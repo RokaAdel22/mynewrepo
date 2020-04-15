15bca145c39cedbe4ed1d3f78366903fc5b4c640
Roka Adel <rokaadel808@gamil.com>
2020-04-15 21:58:14 +0200

    initial commit

---
+++ myApp/myAPP (2).java
@@ -0,0 +1,28 @@
+/**
+ * 
+ */
+package myApp;
+
+/**
+ * @author Roka Adel
+ *
+ */
+public class myAPP {
+
+	/**
+	 * 
+	 */
+	public myAPP() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hellow git");
+
+	}
+
+}
---
+++ myApp2/myApp2.java
@@ -0,0 +1,28 @@
+/**
+ * 
+ */
+package myApp2;
+
+/**
+ * @author Roka Adel
+ *
+ */
+public class myApp2 {
+
+	/**
+	 * 
+	 */
+	public myApp2() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hellow world");
+
+	}
+
+}
